d94989c72a38f73b3017127bc0994e889be63ebe
Saiful Islam <tosaiful@live.com>
2019-01-14 14:23:55 +0100

    adding first practise file

---
+++ script.js
@@ -0,0 +1,14 @@
+function myFunction(a,b){
+    return a * b;
+}
+
+console.log (myFunction(4,6));
+
+
+var x = myCalc(87,3);
+function myCalc(a, b){
+    return a * b;
+}
+
+alert ('Please input something in the next dialogue box!');
+console.log(prompt (x) + ' ' + x);
