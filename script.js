1e3fb46dfd164a7333a2dd8c6ab55cd149a8f3fe
Saiful Islam <tosaiful@live.com>
2019-01-14 14:34:30 +0100

    Practise file added.

---
+++ script.js
@@ -0,0 +1,15 @@
+// function example
+function myFunction(a,b){
+    return a * b;
+}
+
+console.log (myFunction(4,6));
+
+// alert, prompt, push
+var x = myCalc(87,3);
+function myCalc(a, b){
+    return a * b;
+}
+
+alert ('Please input something in the next dialogue box!');
+console.log(prompt (x) + ' ' + x);
