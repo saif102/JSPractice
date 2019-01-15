3412d0ea051b04e43292371138e7afcff5d39b0f
Saiful Islam <tosaiful@live.com>
2019-01-15 17:04:14 +0100

    addition/ summary method included

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ function myCalc(a, b){
 
 alert ('Please input something in the next dialogue box!');
 console.log(prompt (x) + ' ' + x);
+
+/***************************************
+Count average using for and while loops
+****************************************/
+
+//for loops
+arrTest = [276, 986, 724, 255, 762];
+
+var sumArr = 0;
+for (i = 0; i < arrTest.length; i++){
+    sumArr += arrTest[i];
+}
+
+console.log(sumArr);
+console.log(sumArr / arrTest.length);
+
+//while lopps
+
+var sumArr = 0;
+var i = 0;
+while(i < arrTest.length){
+    sumArr += arrTest[i];
+    i++;
+}
+
+console.log(sumArr);
+console.log(sumArr / arrTest.length);
