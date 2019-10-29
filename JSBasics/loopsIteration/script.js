bab1ed3198e31a68211402977706c2e18876b860
Saiful Islam <tosaiful@live.com>
2019-10-29 17:49:24 +0100

    loopIteration project added

---
+++ JSBasics/loopsIteration/script.js
@@ -0,0 +1,45 @@
+/**************************
+
+John and his family went to five diffrent restaurants. 
+The bills were 124€, 48€, 268€, 180€ and 42€.
+John like to tip 20% of the bill when the bill is less than 50€, 
+15% when the bill is between 50€ and 200€, 
+and 10% if the bill is more than 200€.
+
+Implement a tip calculator using objects and loops:
+
+1. Create an object with an array for the bill values.
+2. Add a method to calculate the tip.
+3. This method should include a loop to iterate over all the paid 
+bills and do the tip calculations.
+4. As an output, create 
+        a) a new array containing all tips
+        b) an array containing final paid amount (bill + tip).
+
+HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING
+*********************
+
+Mark's family also went on a holiday, going to four different 
+restaurants. The bills were 77€, 375€, 110€ and 45€.
+
+Mark like to tip 20% of the bill when the bill is less than 100€, 
+10% when the bill is between 100€ and 300€, 
+and 25% when the bill is more than 300€ (different from John).
+
+5. Implement the same functionality as before, this time using 
+Mark's tiping rules.
+6. Create a fucntion (not a method) to calculate the average of a given array of tips.
+
+HINT: Loop over the array and in each iteration store the current 
+sum in a variable (starting from 0). After you have the sum of the 
+array, divide it by the number of elements in it (that's how the 
+average is calculated.)
+
+7. Calculate the average tip for each family.
+8. Log to the console which family paid the highest tips on average.
+
+
+****************************/
